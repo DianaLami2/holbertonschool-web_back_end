d80d0660a96e2a9ae9b4dabce240047c43a2bd97
DianaLami2 <dianalami18@gmail.com>
2024-07-08 21:42:37 +0200

    js02

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,7 @@
+export default function getNeighborhoodsList() {
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
+
