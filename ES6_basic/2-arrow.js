c630a6aae9eb7b80cbe92ec47cecd38960bdd7a2
DianaLami2 <dianalami18@gmail.com>
2024-07-08 21:46:00 +0200

    js02

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,15 +1,6 @@
-const getNeighborhoodsList = () => {
-  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-  const addNeighborhood = (newNeighborhood) => {
-    sanFranciscoNeighborhoods.push(newNeighborhood);
-    return sanFranciscoNeighborhoods;
+export default function getNeighborhoodsList() {
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
   };
-
-  return {
-    addNeighborhood
-  };
-};
-
-export default getNeighborhoodsList;
-
+}
