bee73da620606dc5d1981ee8307f8738dcc02541
DianaLami2 <dianalami18@gmail.com>
2024-07-16 23:23:33 +0200

    Promises09

--- ES6_promise/0-main.js
+++ ES6_promise/0-main.js
@@ -2,11 +2,10 @@ import getResponseFromAPI from "./0-promise.js";
 
 const response = getResponseFromAPI();
 
-console.log(response instanceof Promise); // Should log true
+console.log(response instanceof Promise);
 
 response.then((data) => {
   console.log("Response from API:", data);
 }).catch((error) => {
   console.error("Error fetching data from API:", error);
 });
-
--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,15 +1,9 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // Simulating an asynchronous operation (e.g., fetching data from an API)
     setTimeout(() => {
-      // Assuming the operation is successful after 1 second
       resolve("Success data from API");
-
-      // Simulating an error scenario
-      // reject(new Error("Failed to fetch data from API"));
     }, 1000);
   });
 }
 
 export default getResponseFromAPI;
-
