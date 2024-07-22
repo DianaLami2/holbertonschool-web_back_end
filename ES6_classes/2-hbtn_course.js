9baf0ef4944c11cb68c2f22dafb5dc1c1dfe420a
DianaLami2 <dianalami18@gmail.com>
2024-07-22 10:20:43 +0200

    es6classes-projects

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,52 @@
+class HolbertonCourse {
+    constructor(name, length, students) {
+        if (typeof name !== 'string') {
+            throw new TypeError('Name should be a string');
+        }
+        if (typeof length !== 'number') {
+            throw new TypeError('Length should be a number');
+        }
+        if (!Array.isArray(students) || !students.every(s => typeof s === 'string')) {
+            throw new TypeError('Students should be an array of strings');
+        }
+
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        if (typeof newName !== 'string') {
+            throw new TypeError('Name should be a string');
+        }
+        this._name = newName;
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    set length(newLength) {
+        if (typeof newLength !== 'number') {
+            throw new TypeError('Length should be a number');
+        }
+        this._length = newLength;
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set students(newStudents) {
+        if (!Array.isArray(newStudents) || !newStudents.every(s => typeof s === 'string')) {
+            throw new TypeError('Students should be an array of strings');
+        }
+        this._students = newStudents;
+    }
+}
+
+export default HolbertonCourse;
