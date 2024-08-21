6f2a347bf57bc6cff5ac8c5e37caf28864fe88d5
DianaLami2 <dianalami18@gmail.com>
2024-08-21 18:37:41 +0200

    MySQL02

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,12 +1,11 @@
 -- Make a table
 -- Calculate/compute something is always power intensive… better to distribute the load!
 
-SELECT 
-    origin, 
-    SUM(nb_fans) AS total_fans
+SELECT origin AS origin, SUM(fans) AS nb_fans
+
 FROM 
     metal_bands
 GROUP BY 
     origin
 ORDER BY 
-    total_fans DESC;
+    nb_fans DESC;
