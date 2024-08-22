335edc3919df972836534e1db0bbc586264e6841
DianaLami2 <dianalami18@gmail.com>
2024-08-22 16:47:30 +0200

    MySQL03

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- Sort data
+-- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+
+USE metal_bands;
+
+
+CREATE TEMPORARY TABLE band_lifespans AS
+
+SELECT name AS band_name, IFNULL(YEAR(split) - YEAR(formed), YEAR(CURDATE()) - YEAR(formed)) AS lifespan FROM metal_bands WHERE main_style = 'Glam rock';
+
+SELECT band_name, lifespan FROM band_lifespans ORDER BY lifespan DESC;
+
