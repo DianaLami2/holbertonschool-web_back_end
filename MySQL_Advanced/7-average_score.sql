bdfae59cda92c3978c7da9c64544ebd940c0fcc5
DianaLami2 <dianalami18@gmail.com>
2024-08-26 18:07:28 +0200

    MySql07

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,20 +1,16 @@
 -- comment
--- comment 2
 
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
-
-
+CREATE PROCEDURE ComputeAverageScoreForUser(IN input_user_id INT)
 BEGIN
-    UPDATE users
-    SET
-        avarage_user = (
-            SELECT AVG (score) FROM corrections 
-            WHERE corrections.user_id = user_id
-        )
-    WHERE id = user_id
+    UPDATE users 
+    SET average_score = (
+        SELECT AVG(score)
+        FROM corrections
+        WHERE corrections.user_id = input_user_id
+    )
+    WHERE id = input_user_id;
 END $$
 
 DELIMITER ;
