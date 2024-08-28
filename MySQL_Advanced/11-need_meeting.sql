115eb3623366612b33c6471ffa02c4e574d09bab
DianaLami2 <dianalami18@gmail.com>
2024-08-28 21:12:02 +0200

    MySQL Final

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -1,7 +1,15 @@
---comment
-
+-- Creating the view need_meeting
 CREATE VIEW need_meeting AS
-SELECT student_name
-FROM students
-WHERE score < 80
-  AND (last_meeting IS NULL OR last_meeting < CURRENT_DATE - INTERVAL '1 month');
+SELECT 
+    s.student_name
+FROM 
+    students s
+LEFT JOIN 
+    meetings m
+ON 
+    s.student_id = m.student_id
+GROUP BY 
+    s.student_name, s.score
+HAVING 
+    s.score < 80
+    AND (MAX(m.meeting_date) IS NULL OR MAX(m.meeting_date) < DATEADD(month, -1, GETDATE()));
